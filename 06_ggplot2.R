e4fc6233c36e3b498906ecae5e8d7c1722de7585
Yurii Kleban <yurakleban@gmail.com>
2026-01-21 18:18:15 +0200

    fix

--- 06_ggplot2.R
+++ 06_ggplot2.R
@@ -70,7 +70,56 @@ interviews_plotting |>
 
 interviews_plotting |>
   ggplot(aes(x=respondent_wall_type)) +
-  geom_bar(aes(fill = village), position = "dodge")
+  geom_bar(aes(fill = village), position = "dodge") +
+  labs(title = "Respondent Wall Type by Village",
+       x = "Respondent Wall Type",
+       y = "Count of obs.") +
+  facet_wrap(~ village)
 
+percent_wall_type <- interviews_plotting %>%
+  filter(respondent_wall_type != "cement") %>%
+  count(village, respondent_wall_type) %>%
+  group_by(village) %>%
+  mutate(percent = (n / sum(n)) * 100) %>%
+  ungroup()
 
+# facet
+percent_wall_type %>%
+  ggplot(aes(x = respondent_wall_type, y = percent)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title="Proportion of wall type by village",
+       x="Wall Type",
+       y="Percent") +
+  facet_wrap(~ village) +
+  theme_bw()
 
+percent_items <- interviews_plotting %>%
+  group_by(village) %>%
+  summarize(across(bicycle:no_listed_items, ~ sum(.x) / n() * 100)) %>%
+  pivot_longer(bicycle:no_listed_items, names_to = "items", values_to = "percent")
+percent_items
+
+percent_items %>%
+  ggplot(aes(x = village, y = percent)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  facet_wrap(~ items) +
+  labs(title = "Percent of respondents in each village who owned each item",
+       x = "Village",
+       y = "Percent of Respondents") +
+  theme_dark()
+
+library(ggthemes)
+
+my_plot <- percent_items %>%
+  ggplot(aes(x = village, y = percent, fill = village)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  facet_wrap(~ items) +
+  labs(title = "Percent of respondents in each village who owned each item",
+       x = "Village",
+       y = "Percent of Respondents") +
+  theme_excel_new() +
+  scale_fill_excel_new()
+
+my_plot
+
+ggsave("fig_output/ggplot-demo.png", my_plot)
